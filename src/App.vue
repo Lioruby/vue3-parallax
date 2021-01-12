c3d817bc7da548bd8b08e1dae5c04bd6ed13fd1e
Lior Levy <levy.lior.pro@gmail.com>
2021-01-12 22:57:14 +0100

    Parallax component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <vue-parallax :speed="0.25" item-id="ici">
+    <img
+      class="img__background"
+      :src="src1"
+      id="ici"
+    />
+  </vue-parallax>
+  <vue-parallax :speed="0.09" item-id="caca">
+    <h1 class="img__title" id="caca">
+      DISCOVER
+    </h1>
+  </vue-parallax>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueParallax from './components/VueParallax.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    VueParallax
+  },
+  data() {
+    return {
+      src1: "https://images.unsplash.com/photo-1590880449155-b54f958ce314?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80"
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  .img__background {
+    width: 100%;
+    position: absolute;
+    top: 0;
+  }
+  .img__title {
+    position: relative;
+    color: white;
+    display: flex;
+    justify-content: center;
+    margin-top: 2.7em;
+    font-size: 6em;
+    text-shadow: 3px 3px 8px rgba(0,0,0,0.7);
+  }
 </style>
+
+
---
+++ src/components/VueParallax.vue
@@ -0,0 +1,40 @@
+<template>
+  <slot class="caca">
+  </slot>
+</template>
+
+<script>
+  export default {
+    name: 'VueParallax',
+    props: {
+      width: {
+        type: String,
+        required: false
+      },
+      speed: {
+        type: Number,
+        required: true,
+        default: 0
+      },
+      itemId: {
+        type: String,
+        required: false
+      }
+    },
+    methods: {
+      loadSpeedItem() {
+        const el = document.getElementById(this.itemId);
+
+        window.addEventListener('scroll', () => {
+          el.style.transform = `translateY(${window.pageYOffset * this.speed}px)`;
+        });
+      }
+    },
+    mounted() {
+      this.loadSpeedItem();
+      console.log(this.$slots);
+    }
+  };
+</script>
+
+
