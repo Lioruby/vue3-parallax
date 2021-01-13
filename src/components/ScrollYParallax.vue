5e2b2ec03acf326920cdc8b7e7d5fad83216a372
Lior Levy <levy.lior.pro@gmail.com>
2021-01-13 04:03:40 +0100

    Add the down props

--- src/components/ScrollYParallax.vue
+++ src/components/ScrollYParallax.vue
@@ -13,6 +13,11 @@
         type: Number,
         required: true,
         default: 0
+      },
+      down: {
+        type: Boolean,
+        default: false,
+        required: false
       }
     },
     data() {
@@ -23,9 +28,9 @@
     methods: {
       loadSpeedItem() {
         this.el = this.$refs.scrollYParallax;
-
+        const speedCoeff = this.down ? this.speed : -this.speed
         window.addEventListener('scroll', () => {
-          this.el.style.transform = `translateY(${window.pageYOffset * this.speed}px)`;
+          this.el.style.transform = `translateY(${window.pageYOffset * speedCoeff}px)`;
         });
       }
     },
